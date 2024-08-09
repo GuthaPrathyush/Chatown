c029f414d86238aba718b4b52484cc4f5cd3e3f6
jitendra0104 <jaladijitendra1@gmail.com>
2024-08-09 09:06:43 +0530

    changes done

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,25 +1,41 @@
 import '../stylesheets/Chat.css';
-const Chat = (props) => {
-  return (
-      <div className='Chat'>
-        
-        <div className='Chat_header'>
-          <div className='ProfilePic'></div>
-          <div>{props.name}</div>
-        </div>
-        
-        <div className='Chat_messages'>
-          
-        </div>
-        
-        <div className='Chat_input'>
-          <input type='text' value={inputMessage} onChange={handleInputChange} placeholder='Type a message...' />
-          <button onClick={handleSendMessage}>Send</button>
-        </div>
+import { useState } from "react";
 
+const Chat = (props) => {
+  const [inputMessage, setInputMessage] = useState("");
+
+  const handleChange = (event) => {
+    setInputMessage(event.target.value);
+  };
+
+  const handleSendMessage = () => {
+    
+    console.log("Sending message:", inputMessage);
+    setInputMessage("");
+  };
+
+  return (
+    <div className='Chat'>
+      <div className='Chat_header'>
+        <div className='ProfilePic'></div>
+        <div>{props.name}</div>
       </div>
-    )
-}
+      
+      <div className='Chat_messages'>
+        {/* Add logic to display messages */}
+      </div>
+      
+      <div className='Chat_input'>
+        <input 
+          type='text' 
+          value={inputMessage} 
+          onChange={handleChange} 
+          placeholder='Type a message...' 
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
+    </div>
+  );
+};
 
 export default Chat;
-
